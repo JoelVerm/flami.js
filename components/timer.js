b52d3bcd008f1a728716f25c6076325674c43061
Joël Vermeulen <98051242+JoelVerm@users.noreply.github.com>
2022-08-26 20:58:47 +0200

    fix ReRender

--- components/timer.js
+++ components/timer.js
@@ -1,27 +1,27 @@
-export const timer = () => {
-    let lastPauseTime = new Date().getSeconds()
-    let time = 0
-    setInterval(update, 1000)
-    let running = false
-    const value = () => {
-        if (running)
-            return time + new Date().getSeconds() - lastPauseTime
-        return time
-    }
-    const start = () => {
-        running = true
-        lastPauseTime = new Date().getSeconds()
-    }
-    const stop = () => {
-        if (!running) return
-        running = false
-        time += new Date().getSeconds() - lastPauseTime
-    }
-    const reset = () => {
-        if (running) stop()
-        time = 0
-    }
-    return html`
+let lastPauseTime = new Date().getSeconds()
+let time = 0
+setInterval(update, 1000)
+let running = false
+const value = () => {
+    if (running)
+        return time + new Date().getSeconds() - lastPauseTime
+    return time
+}
+const start = () => {
+    running = true
+    lastPauseTime = new Date().getSeconds()
+}
+const stop = () => {
+    if (!running) return
+    running = false
+    time += new Date().getSeconds() - lastPauseTime
+}
+const reset = () => {
+    if (running) stop()
+    time = 0
+}
+
+export const timer = () => html`
         <div>
             <h1>${value}</h1>
             <button onClick=${start}>start</button>
@@ -29,4 +29,3 @@ export const timer = () => {
             <button onClick=${reset}>reset</button>
         </div>
     `
-}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import {timer} from '../components/timer.js'
 
-const inp = () => {
-    return html`<input type="text" onchange=${e => console.log(e.target.value)}></input>`
-}
+const inp = () => html`<input type="text" onchange=${e => console.log(e.target.value)}></input>`
 
-flami`
+flami(() => html`
     <h1 onclick=${update}>
         ${() => Date.now()}
     </h1>
     ${inp()}
     ${timer()}
-`
+`)
